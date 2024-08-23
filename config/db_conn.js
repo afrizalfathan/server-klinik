8b1c1dd6319162def94933a9eae5f0d0ddc28a9c
Afrizal Fathan <rieslinger8@gmail.com>
2024-08-23 21:52:18 +0700

    change the env password

--- config/db_conn.js
+++ config/db_conn.js
@@ -2,7 +2,7 @@ const { Sequelize, DataTypes } = require("sequelize");
 const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USER,
-  "",
+  process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
